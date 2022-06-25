4a374a80631ff1d9fd9928a338184ab365fc557f
eclipse7723 <eclipse7723@gmail.com>
2022-06-25 14:05:15 +0200

    404 page

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import Header from "./components/Header";
 import PokemonsPage from "./pages/PokemonsPage";
 import AuthPage from "./pages/AuthPage";
 import {BrowserRouter as Router, Routes, Route} from "react-router-dom"
+import NotFoundPage from "./pages/NotFoundPage";
 
 function App() {
   return (<>
@@ -11,6 +12,7 @@ function App() {
       <Routes>
         <Route path="/" element={<PokemonsPage/>} />
         <Route path="/auth/*" element={<AuthPage/>} />
+        <Route path="*" element={<NotFoundPage/>} />
       </Routes>
     </Router>
 
---
+++ src/pages/NotFoundPage.js
@@ -0,0 +1,28 @@
+import React from "react";
+import { Alert, Button, Container } from "react-bootstrap";
+import { SVG } from "../utils";
+
+
+export default function NotFoundPage() {
+
+    return (<>
+    <Container className="d-flex flex-column align-items-center justify-content-center">
+        <div className="mb-4 not-found">
+            <span>4</span>
+            {SVG("pokeball", 20, 20)}
+            <span>4</span>
+        </div>
+        <span class="mb-4">Page not found :(</span>
+        <div>
+            <a href="/">
+                <button type="button" class="btn btn-outline-danger">
+                    <i class="bi bi-arrow-left"></i> Go back
+                </button>
+            </a>
+        </div>
+    </Container>
+    
+    </>)
+
+
+}
