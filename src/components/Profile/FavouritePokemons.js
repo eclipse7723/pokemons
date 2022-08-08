fc5f18eaae95ec18ad67e35fb0fbef4bd607a39a
eclipse7723 <eclipse7723@gmail.com>
2022-08-08 03:29:31 +0200

    done favourite pokemons page

--- src/components/Profile/FavouritePokemons.js
+++ src/components/Profile/FavouritePokemons.js
@@ -1,7 +1,17 @@
 import React from 'react'
+import { useAuth } from '../../contexts/AuthContext'
+import PokemonSpecificList from '../PokemonList/PokemonsSpecificList'
+
 
 export default function FavouritePokemons() {
+  const {loginUserData} = useAuth()
+
   return (
-    <div>FavouritePokemons</div>
+      <>
+      
+      { (loginUserData.favourite_pokemons.length === 0 && <p>You haven't favourite pokemons :'(</p>)
+        || <PokemonSpecificList pokemonsIds={loginUserData.favourite_pokemons} /> }
+
+      </>
   )
 }
