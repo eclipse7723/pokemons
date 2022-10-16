bf9fa50c17c778f40d707b68a71c52f9257bd257
eclipse7723 <eclipse7723@gmail.com>
2022-10-16 03:37:48 +0200

    hide if user not authorized, update fav in firestore to sync

--- src/components/FavouriteButton.js
+++ src/components/FavouriteButton.js
@@ -1,22 +1,38 @@
 import React, { useState, useEffect } from "react";
 import { SVG } from "../utils";
+import { useAuth } from "../contexts/AuthContext";
+import { updateUserParams } from "../services/firestore";
 
-const FavouriteButton = ({defaultValue, id, updateFavourite}) => {
-    // defaultValue should be useState value
+const FavouriteButton = ({defaultValue, id}) => {
+    const { currentUser, loginUserData, updateLoginUserData } = useAuth()
     const [fav, setFav] = useState(defaultValue)
-    
-    function handleClick() {
-        let newState = !fav
 
-        setFav(newState)
-        if (typeof(updateFavourite) === 'function') updateFavourite(newState)
-        console.log(`You just click on '${id}': ${newState}, fav=${fav}`)
+    async function updateFavouritePokemon() {
+        updateLoginUserData()
+        let favList = loginUserData.favourite_pokemons
+
+        if (favList.includes(id)) {
+            favList.splice(favList.indexOf(id), 1)
+            setFav(false)
+        } else {
+            favList.push(id)
+            setFav(true)
+        }
+        await updateUserParams(currentUser.uid, {favourite_pokemons: favList})
+        updateLoginUserData()
+    }
+    
+    async function handleClick() {
+        await updateFavouritePokemon()
+        await updateLoginUserData()
     }
 
     useEffect(() => {
         setFav(defaultValue)
     }, [defaultValue])
     
+    if (!loginUserData) return;
+
     return (<>
     <button className="favourite-btn" onClick={handleClick}>
         {(fav && SVG("favorite-1", 20, 20)) || SVG("favorite-0", 20, 20)}
